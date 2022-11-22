6920cb5dcae3b6ee7f867d3c02e4ee7042e65051
sandraG95 <118214786+sandraG95@users.noreply.github.com>
2022-11-22 21:27:04 +0100

    ejemplo1
    AÑADO UN PROGRAMA

--- proyecto2/src/proyecto2/ejemplo1.java
+++ proyecto2/src/proyecto2/ejemplo1.java
@@ -4,6 +4,10 @@ public class ejemplo1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		
+		public static void imprimeSaludo() { 
+System.out.println(“Hei”);	
+}
 
 	}
 
